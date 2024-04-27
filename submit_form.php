24581fadad21a30384d78d7c14be15ed4ab3de39
timing216 <utaro.hayashi@gmail.com>
2024-04-27 09:54:35 -0500

    added sha256

--- submit_form.php
+++ submit_form.php
@@ -2,10 +2,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 // Database connection parameters
-$servername = "localhost"; // Change this to your database server name
-$username = "utaro"; // Change this to your database username
-$password = "newPassword123"; // Change this to your database password
-$dbname = "registrationForm"; // Change this to your database name
+$servername = "localhost"; // database server name
+$username = "utaro"; // database username
+$password = "newPassword123"; // database password
+$dbname = "registrationForm"; // database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,16 +16,17 @@ if ($conn->connect_error) {
 }
 
 // Retrieve form data
-$userName = $_POST['userName'];
-$email = $_POST['email'];
-$password2 = $_POST['password2'];
-$message = $_POST['message'];
+$userName = trim($_POST['userName']);
+$email = trim($_POST['email']);
+$password2 = trim($_POST['password2']);
+$password2 = hash('sha256',$password2);
+$message = trim($_POST['message']);
 
 // SQL query to insert data into database
 $sql = "INSERT INTO users(userName, email, password2, message) VALUES ('$userName', '$email', '$password2', '$message')";
 
 if ($conn->query($sql) === TRUE) {
-    // echo "New record created successfully";
+    echo "Rgistration Success";
     header("Location: another_page.php");
     exit(); // prevent further script execution
 } else {
