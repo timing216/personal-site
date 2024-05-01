7d850014b8751aa969edc734437951340e208ac8
timing216 <utaro.hayashi@gmail.com>
2024-05-01 00:43:51 -0500

    changed 404 aage styling

--- 404.php
+++ 404.php
@@ -21,11 +21,15 @@
     </header>
 
     <main>
-        <h1>404 Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist. You can return to our <a href="/" style="color: rgb(0, 179, 255);">homepage</a>.</p>
-</body>
-</main>
 
+        <body style="text-align: center;">
+            <h1>404 Page Not Found</h1>
+            <h2>Sorry, the page you are looking for does not exist.<br> You can return to our <a href="/" style="color: rgb(0, 179, 255);">homepage</a>.</h2>
+        </body>
+
+    </main>
+
+</body>
 <footer>
     <div class="footer1" style="color:RGB(1,173,226);">
         // FLATIRON SCHOOL<br />
